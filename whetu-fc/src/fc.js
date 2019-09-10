5a7d39db99384bce99b0331513afd7e3007b61af
kiri-tavaga <kiri.tavaga@icloud.com>
2019-09-10 21:18:05 +1200

    add images

---
+++ whetu-fc/src/fc.js
@@ -0,0 +1,12 @@
+export default {'quiz1':
+  [{'id': 1, 'image': 'images/apple.jpeg', 'Answer': 'Apple'},
+    {'id': 2, 'image': 'images/banana.png', 'Answer': 'Banana'},
+    {'id': 3, 'image': 'images/cherry.png', 'Answer': 'Cherry'},
+    {'id': 4, 'image': 'images/grape.png', 'Answer': 'Grape'},
+    {'id': 5, 'image': 'images/lemon.jpeg', 'Answer': 'Lemon'},
+    {'id': 6, 'image': 'images/lime.jped', 'Answer': 'Lime'},
+    {'id': 7, 'image': 'images/orange.png', 'Answer': 'Orange'},
+    {'id': 8, 'image': 'images/peach.jpeg', 'Answer': 'Peach'},
+    {'id': 9, 'image': 'images/tomato.png', 'Answer': 'Tomato'},
+    {'id': 10, 'image': 'images/pear.jpeg', 'Answer': 'Pear'},]
+}
